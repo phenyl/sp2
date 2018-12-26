0e1eae08691ddf5ec194aaa0b264f4b8da7d9b40
Shin Suzuki <shin.suzuki@cureapp.jp>
2018-12-26 18:10:37 +0900

    style(ESLint): Add some rules to .eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,4 +2,9 @@ module.exports = {
   parser: "typescript-eslint-parser",
   plugins: ["prettier", "typescript"],
   extends: ["eslint:recommended"],
+  rules: {
+    "no-unused-vars": "off",
+    "typescript/no-unused-vars": ["error"],
+    "prefer-arrow-callback": ["error"],
+  },
 };
