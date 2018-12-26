12e73bc1882bf1377a2b5e5176168eafa8c66cc2
Shin Suzuki <shin.suzuki@cureapp.jp>
2018-12-26 18:27:15 +0900

    feat(format): Add DocumentPath and its handlers

---
+++ modules/format/src/common/document-path.ts
@@ -0,0 +1,129 @@
+/**
+ * DocumentPath is a formatted string.
+ * It's used to express the location of the specific value in nested objects.
+ * AWS DynamoDB uses the format.
+ *
+ * For example,
+ *   const obj = { users: [ { name: 'shinout' } ] }
+ * the DocumentPath to express the location of "shinout" is "users[0].name"
+ *
+ * Detailed format is written here: http://docs.aws.amazon.com/amazondynamodb/latest/developerguide/Expressions.Attributes.html#Expressions.Attributes.NestedElements.DocumentPathExamples
+ */
+export type DocumentPath = string;
+
+/**
+ * DotNotationString is also a formatted string to express the location in nested objects.
+ * The only difference between DotNotationString and DocumentPath is the way to handle array items.
+ * MongoDB uses the format.
+ *
+ * For example,
+ *   const obj = { users: [ { name: 'shinout' } ] }
+ * the DotNotationString to express the location of "shinout" is "users.0.name", whereas
+ * the DocumentPath to express the location of "shinout" is "users[0].name".
+ *
+ * Detailed format is written here: http://docs.aws.amazon.com/amazondynamodb/latest/developerguide/Expressions.Attributes.html#Expressions.Attributes.NestedElements.DocumentPathExamples
+ */
+export type DotNotationString = string;
+
+type Attribute = string | number;
+
+/**
+ * @public
+ * Parse DocumentPath into an array of property names.
+ */
+export function parseDocumentPath(docPath: DocumentPath): Array<Attribute> {
+  /*
+   * This is workaround for negative lookbehind regular expression.
+   * Some JS runtimes haven't implemented the feature determined by ES2018.
+   * "$$$" is a temporary replacer of ".".
+   */
+  return docPath
+    .split(/\\./)
+    .join("$$$")
+    .split(/[.[]/)
+    .map(attribute =>
+      attribute.charAt(attribute.length - 1) === "]"
+        ? parseInt(attribute.slice(0, -1))
+        : unescapePathDelimiter(attribute.split("$$$").join("\\."))
+    );
+}
+
+/**
+ * @public
+ * Create DocumentPath from arguments.
+ */
+export function createDocumentPath(...attributes: Attribute[]): DocumentPath {
+  const joined = attributes.reduce(
+    (docPath, attr) =>
+      typeof attr === "string"
+        ? `${docPath}.${escapePathDelimiter(attr)}`
+        : `${docPath}[${attr.toString()}]`,
+    ""
+  );
+  // @ts-ignore "joined" must be string here.
+  return joined.charAt(0) === "." ? joined.slice(1) : joined;
+}
+
+/**
+ * @public
+ * Convert DocumentPath to DotNotationString, expected to be used to pass to MongoDB.
+ */
+export function convertToDotNotationString(
+  docPath: DocumentPath
+): DotNotationString {
+  return docPath.replace(/\[(\d{1,})\]/g, ".$1") as DotNotationString;
+}
+
+function escapePathDelimiter(attr: string | number): string | number {
+  return typeof attr === "number" ? attr : attr.replace(/\./g, "\\.");
+}
+
+function unescapePathDelimiter(attr: string | number): string | number {
+  return typeof attr === "number" ? attr : attr.replace(/\\\./, ".");
+}
+
+/**
+ * @public
+ * Get the value in the object at the DocumentPath.
+ */
+export function getNestedValue(
+  obj: Object,
+  docPath: DocumentPath,
+  noNullAccess?: boolean
+): any {
+  const keys = parseDocumentPath(docPath);
+  let currentObj = obj;
+  for (const key of keys) {
+    try {
+      // @ts-ignore allow nullable access here.
+      currentObj = currentObj[key];
+    } catch (e) {
+      if (noNullAccess) {
+        throw new Error(
+          `Cannot get value by the document path: "${docPath}". The property "${key}" is not found in the upper undefined object.`
+        );
+      }
+      return undefined;
+    }
+  }
+  return currentObj;
+}
+
+/**
+ * @public
+ * Check if the object has the DocumentPath.
+ */
+export function hasOwnNestedProperty(
+  obj: Object,
+  docPath: DocumentPath
+): boolean {
+  let currentObj = obj;
+  for (const key of parseDocumentPath(docPath)) {
+    if (!currentObj || !currentObj.hasOwnProperty(key)) {
+      return false;
+    }
+    // @ts-ignore this access must be safe.
+    currentObj = currentObj[key];
+  }
+  return true;
+}
---
+++ modules/format/test/common/document-path.test.ts
@@ -0,0 +1,117 @@
+/* eslint-env mocha */
+import {
+  convertToDotNotationString,
+  createDocumentPath,
+  getNestedValue,
+  hasOwnNestedProperty,
+  parseDocumentPath,
+} from "../../src/common/document-path";
+
+import assert from "assert";
+
+describe("parseDocumentPath", () => {
+  it("parses documentPath", () => {
+    const docPath = "user.favorites[1].music[30000]";
+    const attributes = parseDocumentPath(docPath);
+    assert.deepEqual(attributes, ["user", "favorites", 1, "music", 30000]);
+  });
+  it("not parses escaped path", () => {
+    const docPath = "user.name\\.profile.favorites[1].music[30000]";
+    const attributes = parseDocumentPath(docPath);
+    assert.deepEqual(attributes, [
+      "user",
+      "name.profile",
+      "favorites",
+      1,
+      "music",
+      30000,
+    ]);
+  });
+});
+
+describe("convertToDotNotation", () => {
+  it("converts docPath to dotNotationString", () => {
+    const docPath = "user.favorites[1].music[30000]";
+    const dotnotationString = convertToDotNotationString(docPath);
+    assert(dotnotationString === "user.favorites.1.music.30000");
+  });
+});
+
+describe("createDocumentPath", () => {
+  it("converts attributes list to DocumentPath", () => {
+    const attrs = [
+      "users",
+      1,
+      "favorites",
+      "musics",
+      24,
+      "title",
+      "3",
+      "value",
+    ];
+    const docPath = createDocumentPath(...attrs);
+    assert(docPath === "users[1].favorites.musics[24].title.3.value");
+  });
+  it("returns empty string when no attribute list is given", () => {
+    assert(createDocumentPath() === "");
+  });
+  it("converts dot included attribute to escaped path", () => {
+    const attrs = ["user", "name.profile", "favorites", 1, "music", 30000];
+    const docPath = createDocumentPath(...attrs);
+    assert(docPath === "user.name\\.profile.favorites[1].music[30000]");
+  });
+});
+
+describe("getNestedValue", () => {
+  const obj = {
+    foo: { bar: [{}, {}, { baz1: false, baz2: null }] },
+    foo2: undefined,
+  };
+
+  it("returns a nested value", () => {
+    assert(getNestedValue(obj, "foo") === obj.foo);
+    assert(getNestedValue(obj, "foo.bar") === obj.foo.bar);
+    assert(getNestedValue(obj, "foo.bar[0]") === obj.foo.bar[0]);
+    assert(getNestedValue(obj, "foo.bar[1]") === obj.foo.bar[1]);
+    assert(getNestedValue(obj, "foo.bar[2]") === obj.foo.bar[2]);
+    assert(getNestedValue(obj, "foo.bar[2].baz1") === obj.foo.bar[2].baz1);
+    assert(getNestedValue(obj, "foo.bar[2].baz2") === obj.foo.bar[2].baz2);
+    assert(getNestedValue(obj, "foo2") === obj.foo2);
+  });
+
+  it("returns undefined when nested value is not found.", () => {
+    assert(getNestedValue(obj, "a.b.c.d.e") === undefined);
+  });
+
+  it("throws error when the 3rd argument is true and nested value is not found.", () => {
+    assert.throws(
+      () => getNestedValue(obj, "a.b.c.d.e", true),
+      /Cannot get value/
+    );
+  });
+});
+
+describe("hasOwnNestedProperty", () => {
+  const obj = {
+    foo: { bar: [{}, {}, { baz1: false, baz2: null }] },
+    foo2: undefined,
+  };
+
+  it("returns true when it has own nested property", () => {
+    assert(hasOwnNestedProperty(obj, "foo") === true);
+    assert(hasOwnNestedProperty(obj, "foo.bar") === true);
+    assert(hasOwnNestedProperty(obj, "foo.bar[0]") === true);
+    assert(hasOwnNestedProperty(obj, "foo.bar[1]") === true);
+    assert(hasOwnNestedProperty(obj, "foo.bar[2]") === true);
+    assert(hasOwnNestedProperty(obj, "foo.bar[2].baz1") === true);
+    assert(hasOwnNestedProperty(obj, "foo.bar[2].baz2") === true);
+    assert(hasOwnNestedProperty(obj, "foo2") === true);
+  });
+
+  it("returns false when it doesn't have own nested property", () => {
+    assert(hasOwnNestedProperty(obj, "bar") === false);
+    assert(hasOwnNestedProperty(obj, "foo.baz") === false);
+    assert(hasOwnNestedProperty(obj, "foo.bar[3]") === false);
+    assert(hasOwnNestedProperty(obj, "foo.bar[2].baz3") === false);
+  });
+});
