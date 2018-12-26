db248b6bccaafcc4a7b6b0f8fac8fa4e7b195410
Shin Suzuki <shin.suzuki@cureapp.jp>
2018-12-26 18:46:43 +0900

    feat(retriever): Add index

---
+++ modules/retriever/src/index.ts
@@ -0,0 +1,22 @@
+export { checkCondition, retrieve } from "./retriever";
+
+export {
+  AndFindOperation,
+  ArrayOperatorName,
+  BSONTypeNumber,
+  BSONTypeString,
+  BitwiseQueryOperator,
+  ComparisonQueryOperator,
+  ElementQueryOperator,
+  EqCondition,
+  EvaluationQueryOperator,
+  FindOperation,
+  GeospatialQueryOperator,
+  LogicalQueryOperator,
+  NorFindOperation,
+  OrFindOperation,
+  QueryCondition,
+  QueryOperator,
+  SimpleFindOperation,
+  getNestedValue,
+} from "@sp2/format";
