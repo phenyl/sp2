0236ef5fe9ea3cfe95471bcc5b6fb7a907711060
Shin Suzuki <shin.suzuki@cureapp.jp>
2019-02-13 01:50:23 +0900

    fix(format): Fix bug: mergeUpdateOperations should not make values plainized

--- modules/format/src/updating/merge-update-operations.ts
+++ modules/format/src/updating/merge-update-operations.ts
@@ -2,10 +2,11 @@ import {
   GeneralRegularUpdateOperation,
   NonBreakingRegularUpdateOperation,
   NonBreakingUpdateOperationOrSetOperand,
+  RegularUpdateOperand,
   UpdateOperationOrSetOperand,
+  UpdateOperator,
 } from "./update-operation";
 
-import deepMerge from "deepmerge";
 import { normalizeUpdateOperation } from "./normalize-update-operation";
 
 /**
@@ -23,5 +24,135 @@ export function mergeUpdateOperations(
 export function mergeUpdateOperations(
   ...operationList: Object[]
 ): GeneralRegularUpdateOperation {
-  return deepMerge.all(operationList.map(normalizeUpdateOperation));
+  return mergeNormalizedUpdateOperations(
+    ...operationList.map(normalizeUpdateOperation)
+  );
+}
+
+export function mergeNormalizedUpdateOperations(
+  ...operations: GeneralRegularUpdateOperation[]
+): GeneralRegularUpdateOperation {
+  const ret: GeneralRegularUpdateOperation = {};
+  operations.forEach(operation => {
+    Object.entries(operation).forEach(([k, v]) => {
+      if (!ret.hasOwnProperty(k)) {
+        // @ts-ignore
+        ret[k] = Object.assign({}, v);
+      } else if (k === "$push" || k === "$addToSet") {
+        // @ts-ignore
+        ret[k] = mergeArrayOperand(ret[k], v);
+      } else if (k === "$inc") {
+        // @ts-ignore
+        ret[k] = mergeIncOperand(ret[k], v);
+      } else if (k === "$min") {
+        // @ts-ignore
+        ret[k] = mergeMinOperand(ret[k], v);
+      } else if (k === "$max") {
+        // @ts-ignore
+        ret[k] = mergeMaxOperand(ret[k], v);
+      } else if (k === "$mul") {
+        // @ts-ignore
+        ret[k] = mergeMulOperand(ret[k], v);
+      } else {
+        // @ts-ignore
+        ret[k] = overwriteOperand(ret[k], v);
+      }
+    });
+  });
+  return ret;
+}
+
+function mergeIncOperand<OP extends "$inc">(
+  operand1: RegularUpdateOperand<OP>,
+  operand2: RegularUpdateOperand<OP>
+): RegularUpdateOperand<OP> {
+  const ret: RegularUpdateOperand<OP> = Object.assign({}, operand1);
+
+  Object.entries(operand2).forEach(([k, v]) => {
+    if (ret.hasOwnProperty(k)) {
+      ret[k] = ret[k] + v;
+    } else {
+      ret[k] = v;
+    }
+  });
+  return ret;
+}
+
+function mergeMinOperand<OP extends "$min">(
+  operand1: RegularUpdateOperand<OP>,
+  operand2: RegularUpdateOperand<OP>
+): RegularUpdateOperand<OP> {
+  const ret: RegularUpdateOperand<OP> = Object.assign({}, operand1);
+
+  Object.entries(operand2).forEach(([k, v]) => {
+    if (ret.hasOwnProperty(k)) {
+      // @ts-ignore
+      ret[k] = Math.min(ret[k], v);
+    } else {
+      ret[k] = v;
+    }
+  });
+  return ret;
+}
+
+function mergeMaxOperand<OP extends "$max">(
+  operand1: RegularUpdateOperand<OP>,
+  operand2: RegularUpdateOperand<OP>
+): RegularUpdateOperand<OP> {
+  const ret: RegularUpdateOperand<OP> = Object.assign({}, operand1);
+
+  Object.entries(operand2).forEach(([k, v]) => {
+    if (ret.hasOwnProperty(k)) {
+      // @ts-ignore
+      ret[k] = Math.max(ret[k], v);
+    } else {
+      ret[k] = v;
+    }
+  });
+  return ret;
+}
+
+function mergeMulOperand<OP extends "$mul">(
+  operand1: RegularUpdateOperand<OP>,
+  operand2: RegularUpdateOperand<OP>
+): RegularUpdateOperand<OP> {
+  const ret: RegularUpdateOperand<OP> = Object.assign({}, operand1);
+
+  Object.entries(operand2).forEach(([k, v]) => {
+    if (ret.hasOwnProperty(k)) {
+      ret[k] = ret[k] * v;
+    } else {
+      ret[k] = v;
+    }
+  });
+  return ret;
+}
+
+function overwriteOperand<
+  OP extends Exclude<
+    UpdateOperator,
+    "$push" | "$addToSet" | "$inc" | "$mul" | "$min" | "$max"
+  >
+>(
+  operand1: RegularUpdateOperand<OP>,
+  operand2: RegularUpdateOperand<OP>
+): RegularUpdateOperand<OP> {
+  return Object.assign({}, operand1, operand2);
+}
+
+function mergeArrayOperand<OP extends "$push" | "$addToSet">(
+  arrayOperand1: RegularUpdateOperand<OP>,
+  arrayOperand2: RegularUpdateOperand<OP>
+): RegularUpdateOperand<OP> {
+  const ret: RegularUpdateOperand<OP> = Object.assign({}, arrayOperand1);
+
+  Object.entries(arrayOperand2).forEach(([k, v]) => {
+    if (ret.hasOwnProperty(k)) {
+      const $each = [...ret[k].$each, ...v.$each];
+      ret[k] = Object.assign({}, ret[k], { $each });
+    } else {
+      ret[k] = v;
+    }
+  });
+  return ret;
 }
--- modules/format/test/updating/merge-update-operations.test.ts
+++ modules/format/test/updating/merge-update-operations.test.ts
@@ -22,24 +22,145 @@ describe("mergeUpdateOperations", () => {
       },
     });
   });
-  it("merges push operations deeply", () => {
+
+  it("merges $push operands deeply", () => {
     const op1 = { $push: { hobby: "tennis" } };
     const op2 = { $push: { hobby: "jogging" } };
-    const mergedOperation = mergeUpdateOperations(op1, op2);
+    const op3 = { $push: { hobby: "skiing" } };
+    const mergedOperation = mergeUpdateOperations(op1, op2, op3);
     assert.deepStrictEqual(mergedOperation, {
       $push: {
-        hobby: { $each: ["tennis", "jogging"] },
+        hobby: { $each: ["tennis", "jogging", "skiing"] },
       },
     });
   });
-  it("merges objects to be set deeply", () => {
+
+  it("merges $addToSet operands deeply", () => {
+    const op1 = { $addToSet: { hobby: "tennis" } };
+    const op2 = { $addToSet: { hobby: "jogging" } };
+    const op3 = { $addToSet: { hobby: "skiing" } };
+    const mergedOperation = mergeUpdateOperations(op1, op2, op3);
+    assert.deepStrictEqual(mergedOperation, {
+      $addToSet: {
+        hobby: { $each: ["tennis", "jogging", "skiing"] },
+      },
+    });
+  });
+
+  it("sums up $inc operands", () => {
+    const op1 = { $inc: { age: 1 } };
+    const op2 = { $inc: { age: 2 } };
+    const op3 = { $inc: { age: 5 } };
+    const mergedOperation = mergeUpdateOperations(op1, op2, op3);
+    assert.deepStrictEqual(mergedOperation, {
+      $inc: {
+        age: 1 + 2 + 5,
+      },
+    });
+  });
+
+  it("chooses the minimum one in all $min operands", () => {
+    const op1 = { $min: { price: 1000 } };
+    const op2 = { $min: { price: 1200 } };
+    const op3 = { $min: { price: 1800 } };
+    const mergedOperation = mergeUpdateOperations(op1, op2, op3);
+    assert.deepStrictEqual(mergedOperation, {
+      $min: {
+        price: 1000,
+      },
+    });
+  });
+
+  it("sums up $max operands", () => {
+    const op1 = { $max: { price: 1300 } };
+    const op2 = { $max: { price: 700 } };
+    const op3 = { $max: { price: 1200 } };
+    const mergedOperation = mergeUpdateOperations(op1, op2, op3);
+    assert.deepStrictEqual(mergedOperation, {
+      $max: {
+        price: 1300,
+      },
+    });
+  });
+
+  it("multiplies $mul operands", () => {
+    const op1 = { $mul: { point: 3 } };
+    const op2 = { $mul: { point: 2 } };
+    const op3 = { $mul: { point: 7 } };
+    const mergedOperation = mergeUpdateOperations(op1, op2, op3);
+    assert.deepStrictEqual(mergedOperation, {
+      $mul: {
+        point: 3 * 2 * 7,
+      },
+    });
+  });
+
+  it("overwrites former objects to the latter one if the same key is given in $set operand", () => {
     const op1 = { name: { first: "John" } };
-    const op2 = { name: { last: "Doe" } };
-    const mergedOperation = mergeUpdateOperations(op1, op2);
+    const op2 = { name: { middle: "M" } };
+    const op3 = { name: { last: "Doe" } };
+    const mergedOperation = mergeUpdateOperations(op1, op2, op3);
     assert.deepStrictEqual(mergedOperation, {
       $set: {
-        name: { first: "John", last: "Doe" },
+        name: { last: "Doe" },
       },
+    });
+  });
+
+  it("doesn't convert instances to plain objects", () => {
+    class Person {
+      name: string;
+      constructor(name: string) {
+        this.name = name;
+      }
+    }
+    const op1 = { person: new Person("Shin") };
+    const op2 = { person: new Person("John") };
+    const mergedOperation = mergeUpdateOperations(op1, op2);
+    assert(mergedOperation.$set!.person instanceof Person);
+    assert.deepStrictEqual(mergedOperation, {
+      $set: { person: new Person("John") },
+    });
+  });
+
+  it("merges $push, $addToSet, $inc and $mul operands and overwrites other operands", () => {
+    const op1 = {
+      $addToSet: { "profile.hobbies": "tennis" },
+      $push: { "profile.nationalities": "Japan" },
+      $inc: { "profile.age": 1 },
+      $mul: { "profile.point.value": 5 },
+      $set: { "profile.name.first": "Shin", "profile.name.last": "Suzuki" },
+    };
+    const op2 = {
+      $addToSet: { "profile.hobbies": "jogging" },
+      $push: {
+        "profile.nationalities": { $each: ["United States", "Thailand"] },
+        "profile.favoriteFoods": { $each: ["sukiyaki", "sushi"] },
+      },
+      $inc: { "profile.age": 3 },
+      $mul: { "profile.point.value": 6 },
+      $set: { "profile.name.last": "Doe" },
+    };
+    const mergedOperation = mergeUpdateOperations(op1, op2);
+    assert.deepStrictEqual(mergedOperation, {
+      $addToSet: {
+        "profile.hobbies": { $each: ["tennis", "jogging"] },
+      },
+      $push: {
+        "profile.nationalities": {
+          $each: ["Japan", "United States", "Thailand"],
+        },
+        "profile.favoriteFoods": {
+          $each: ["sukiyaki", "sushi"],
+        },
+      },
+      $inc: {
+        "profile.age": 1 + 3,
+      },
+      $mul: {
+        "profile.point.value": 5 * 6,
+      },
+      $set: { "profile.name.first": "Shin", "profile.name.last": "Doe" },
     });
   });
 });
