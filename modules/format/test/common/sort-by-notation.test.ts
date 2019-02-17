6891186ccad44603f787a3fc46c60230158e9769
Shin Suzuki <shin.suzuki@cureapp.jp>
2019-02-18 03:37:19 +0900

    test(format): Add more test for SortNotation

--- modules/format/test/common/sort-by-notation.test.ts
+++ modules/format/test/common/sort-by-notation.test.ts
@@ -7,7 +7,15 @@ import {
 import assert from "assert";
 
 describe("sortByNotation", () => {
-  it("sorts string and number, 1 is ASC and -1 is DESC", () => {
+  it("sorts values by direction, 1 is ASC and -1 is DESC", () => {
+    const sortNotation: SortNotation = -1;
+    const data = ["foo", "bar", "baz", "foo"];
+
+    const sortedArr = sortByNotation(data, sortNotation);
+    assert.deepEqual(sortedArr, ["foo", "foo", "baz", "bar"]);
+  });
+
+  it("sorts given props (string and number), 1 is ASC and -1 is DESC", () => {
     const sortNotation: SortNotation = {
       name: 1,
       age: -1,
